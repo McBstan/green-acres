32e03b73069507dba82c999c9cca11dce0469f45
Blake Stanley <stanley_blake@yahoo.com>
2023-11-17 20:37:51 -0600

    Buildout for Contact Form

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../App.css';
 import Contact from '../Contact';
-import ContactForm from '../ContactForm';
+// import ContactForm from '../ContactForm';
 import Footer from '../Footer';
 
 export default function SignUp() {
