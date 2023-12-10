dd65ea6908b6bb9b79f75dfa37113a8ec4993d99
Blake Stanley <stanley_blake@yahoo.com>
2023-12-10 00:55:18 -0600

    Subscription Toaster

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,37 @@
-import React from 'react';
-import styles from './Footer.module.css'; // Assuming you use CSS Modules
+import React, { useState } from 'react';
+import styles from './Footer.module.css';
 import Link from 'next/link';
 
 function Footer() {
-  // Function to handle the form submission
+  const [isToastVisible, setToastVisible] = useState(false);
+  const [toastMessage, setToastMessage] = useState('');
+  const [email, setEmail] = useState('');
+
+  const showToast = (message, duration = 3000) => {
+    setToastMessage(message);
+    setToastVisible(true);
+    setTimeout(() => {
+      setToastVisible(false);
+    }, duration);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-
     const formData = new FormData(e.target);
     const formProps = Object.fromEntries(formData);
 
-    // Post data using fetch to Netlify
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: new URLSearchParams(formProps).toString(),
     })
-    .then(() => console.log('Form successfully submitted'))
-    .catch((error) => alert(error));
+    .then(() => {
+      showToast('Subscription successful!');
+      setEmail('');
+    })
+    .catch((error) => {
+      showToast('Subscription failed. Please try again.');
+    });
   };
 
   return (
@@ -29,19 +43,28 @@ function Footer() {
         <p className={styles.footerSubscriptionText}>
           You can unsubscribe at any time.
         </p>
-        <div className={styles.inputAreas}>
-          <form name="newsletter-signup" method="POST" data-netlify="true" onSubmit={handleSubmit}>
-            <input type="hidden" name="form-name" value="newsletter-signup" />
-            <input
-              className={styles.footerInput}
-              name='email'
-              type='email'
-              placeholder='Your Email'
-              required
-            />
-          <button type="submit" className={styles.buttonOutline}>Subscribe</button>
-          </form>
+
+        {isToastVisible && (
+        <div className={`${styles.toast} ${isToastVisible ? styles.show : ''}`}>
+          {toastMessage}
         </div>
+      )}
+
+        <div className={styles.inputAreas}>
+        <form name="newsletter-signup" method="POST" data-netlify="true" onSubmit={handleSubmit}>
+          <input type="hidden" name="form-name" value="newsletter-signup" />
+          <input
+            className={styles.footerInput}
+            name='email'
+            type='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder='Your Email'
+            required
+          />
+          <button type="submit" className={styles.buttonOutline}>Subscribe</button>
+        </form>
+      </div>
       </section>
 
       <div className={styles.footerLinks}>
@@ -50,7 +73,6 @@ function Footer() {
         </div>
         <div className={styles.footerLinkWrapper}>
           <div className={styles.footerLinkItems}>
-            {/* External Links should still use 'a' tags */}
             <a href='https://instagram.com/greenacres_yardcare' target="_blank" rel="noopener noreferrer">Instagram</a>
             <a href='https://www.facebook.com/GreenAcresYardcare' target="_blank" rel="noopener noreferrer">Facebook</a>
           </div>
